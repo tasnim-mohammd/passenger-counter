1f0f46b82d49159cc5a054d2e459155d8dc5963e
Tasnim <s12028850@stu.najah.edu>
2023-08-03 20:07:27 +0300

    Add new features: reset decrement

--- index.js
+++ index.js
@@ -13,3 +13,26 @@ function save() {
     countEl.textContent = 0
     count = 0
 }
+
+function decrement() {
+    if(count > 0)
+    {
+        count -= 1
+        countEl.textContent = count
+    }
+    else
+    {
+        let errorParagraph = document.getElementById("error")
+        errorParagraph.textContent = "You can't decrement when the counter is zero, it should be more than that."
+
+        setTimeout(function() {
+            errorParagraph.textContent = "";
+        }, 5000); 
+    }   
+
+}
+
+function reset(){
+    count = 0
+    saveEl.textContent = "Previous entries: "
+}
